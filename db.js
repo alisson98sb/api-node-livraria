e934c7f5b8ef47c4540ab5712674f96add0c5fae
alisson98sb <alisson98sb@gmail.com>
2023-09-30 22:39:19 -0300

    add eslint

--- db.js
+++ db.js
@@ -1,69 +1,69 @@
-import pkg from 'pg';
-import 'dotenv/config';
+import pkg from "pg";
+import "dotenv/config";
 
 const { Pool } = pkg;
 
 async function connect() {
-    if(global.connection){
-        return global.connection.connect();
-    }
+  if(global.connection){
+    return global.connection.connect();
+  }
 
 
-    const pool = new Pool({
-        user: process.env.PG_USER,
-        host: 'localhost',
-        database: 'books-api',
-        password: process.env.PG_PASSWORD,
-        port: 5432, // Porta padrão do PostgreSQL
-    });
+  const pool = new Pool({
+    user: process.env.PG_USER,
+    host: "localhost",
+    database: "books-api",
+    password: process.env.PG_PASSWORD,
+    port: 5432, // Porta padrão do PostgreSQL
+  });
 
-    const client = await pool.connect();
-    console.log("Criou o pool de conexão");
+  const client = await pool.connect();
+  console.log("Criou o pool de conexão");
 
-    // const res = await client.query("select now()");
-    // console.log(res.rows[0])
-    client.release();
+  // const res = await client.query("select now()");
+  // console.log(res.rows[0])
+  client.release();
 
-    global.connection = pool;
-    return pool.connect();
+  global.connection = pool;
+  return pool.connect();
 }
 
 connect();
 
 
 async function selectBooks(){
-    const client = await connect();
-    const res = await client.query("SELECT * FROM node_livros");
-    return res.rows;
+  const client = await connect();
+  const res = await client.query("SELECT * FROM node_livros");
+  return res.rows;
 }
 
 async function selectBook(id){
-    const client = await connect();
-    const sql = "SELECT * FROM node_livros WHERE id=$1";
-    const values = [id];
-    const res = await client.query(sql, values);
-    return res.rows;
+  const client = await connect();
+  const sql = "SELECT * FROM node_livros WHERE id=$1";
+  const values = [id];
+  const res = await client.query(sql, values);
+  return res.rows;
 }
 
 async function insertBook(customer){
-    const client = await connect();
-    const sql = "INSERT INTO node_livros(id, titulo, autor) VALUES ($1, $2, $3)";
-    const values = [customer.id, customer.titulo, customer.autor];
-    await client.query(sql, values);
+  const client = await connect();
+  const sql = "INSERT INTO node_livros(id, titulo, autor) VALUES ($1, $2, $3)";
+  const values = [customer.id, customer.titulo, customer.autor];
+  await client.query(sql, values);
 }
 
 async function updateBook(id, customer){
-    const client = await connect();
-    const sql = "UPDATE node_livros SET titulo=$1, autor=$2 WHERE id=$3";
-    const values = [customer.titulo, customer.autor, id];
-    await client.query(sql, values);
+  const client = await connect();
+  const sql = "UPDATE node_livros SET titulo=$1, autor=$2 WHERE id=$3";
+  const values = [customer.titulo, customer.autor, id];
+  await client.query(sql, values);
 }
 
 async function deleteBook(id){
-    const client = await connect();
-    const sql = "DELETE from node_livros WHERE id=$1";
-    const values = [id];
-    await client.query(sql, values);
+  const client = await connect();
+  const sql = "DELETE from node_livros WHERE id=$1";
+  const values = [id];
+  await client.query(sql, values);
 }
 
 export {selectBooks, selectBook, insertBook, updateBook, deleteBook};
--- src/controller/livroController.js
+++ src/controller/livroController.js
@@ -3,53 +3,59 @@ import { deleteBook, insertBook, selectBook, selectBooks, updateBook } from "../
 console.log(deleteBook);
 class LivroController {
     
-    static async listarLivros (req, res) {
-        try {
-            const listarLivros = await selectBooks();
-            res.status(200).json(listarLivros);
-        } catch (erro){
-            res.status(500).json({message: `${erro.message} - falaha na requisição`});
-        }
+  static async listarLivros (req, res) {
+    try {
+      const listarLivros = await selectBooks();
+      res.status(200).json(listarLivros);
+    } catch (erro){
+      res.status(500).json({message: `${erro.message} - falaha na requisição`});
     }
+  }
 
-    static async listarLivroPorId (req, res) {
-        try {
-            const id = req.params.id;
-            const livroEncontrado = await selectBook(id);
-            res.status(200).json(livroEncontrado);
-        } catch (erro){
-            res.status(500).json({message: `${erro.message} - falaha na requisição`});
-        }
+  static async listarLivroPorId (req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await selectBook(id);
+
+      if(livroEncontrado.length != 0) {
+        res.status(200).json(livroEncontrado);
+      } else {
+        res.status(400).send({message: "Id do livro não encontrado"});
+      }
+
+    } catch (erro){
+      res.status(500).send({message: `${erro.message} - Erro interno de servidor`});
     }
+  }
 
-    static async cadastrarLivro (req, res) {
-        try {
-            const novoLivro = await insertBook(req.body);
-            res.status(200).json({ message: "Criado com sucesso", livro:novoLivro});
-        } catch (erro){
-            res.status(500).json({message: `${erro.message} - falaha ao cadastrar livro`});
-        }
+  static async cadastrarLivro (req, res) {
+    try {
+      const novoLivro = await insertBook(req.body);
+      res.status(200).json({ message: "Criado com sucesso", livro:novoLivro});
+    } catch (erro){
+      res.status(500).json({message: `${erro.message} - falaha ao cadastrar livro`});
     }
+  }
 
-    static async atualizarLivro (req, res) {
-        try {
-          const id = req.params.id;
-          await updateBook(id, req.body);
-          res.status(200).json({ message: "livro atualizado" });
-        } catch (erro) {
-          res.status(500).json({ message: `${erro.message} - falha na atualização` });
-        }
-      };
+  static async atualizarLivro (req, res) {
+    try {
+      const id = req.params.id;
+      await updateBook(id, req.body);
+      res.status(200).json({ message: "livro atualizado" });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na atualização` });
+    }
+  }
     
-      static async excluirLivro (req, res) {
-        try {
-          const id = req.params.id;
-          await deleteBook(id);
-          res.status(200).json({ message: "livro excluído com sucesso" });
-        } catch (erro) {
-          res.status(500).json({ message: `${erro.message} - falha na exclusão` });
-        }
-      };
+  static async excluirLivro (req, res) {
+    try {
+      const id = req.params.id;
+      await deleteBook(id);
+      res.status(200).json({ message: "livro excluído com sucesso" });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na exclusão` });
+    }
+  }
 }
 
 export default LivroController;
